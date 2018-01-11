8b7131b208d62988ec091fb8e6f4ea6c58ce7cf6
adam <adam@adamwasserman.net>
2018-01-11 18:09:13 -0500

    Initial commit

---
+++ vt_functions.py
@@ -0,0 +1,257 @@
+#  Python 3.6 modules
+import re
+import ipaddress
+import urllib.request
+import urllib.parse
+import urllib.error
+import json
+import time
+
+#  MIT licensed 3rd party modules
+import riprova
+
+#  import config data and grab api_key
+import config as cfg
+api_key = cfg.api_key
+
+#  ***Tests to determine type of lookup value***
+
+#  Is lookup_value an IP address?
+def is_ip(lookup_value):
+    try:
+        if ipaddress.ip_network(lookup_value,strict=False) == ipaddress.ip_network(lookup_value,strict=False):
+            return True
+    except ValueError:
+        return False
+
+#  Is lookup_value a URL? (taken from Django, modified ot accept long TLDs)
+def is_url(lookup_value):
+    regex = re.compile(
+        r'^https?://'   # has http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,20}\.?|'  # has a domain OR
+        r'localhost|'   # is localhost OR
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # has an ip
+        r'(?::\d+)?'   # may have port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    if lookup_value is not None and regex.search(lookup_value):
+        return True
+
+#  Is lookup_value a domain?
+def is_domain(lookup_value):
+    if len(lookup_value) > 255:
+        return False
+    if lookup_value[-1] == ".":
+        lookup_value = lookup_value[:-1]  # strip exactly one dot from the right, if present
+    regex = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(regex.match(x) for x in lookup_value.split("."))
+
+#  ***Functions to perform the three different kinds of lookups, plus one to force VT to re-scan a URL***
+
+#  Riprova error logger
+def on_retry(err, next_try):
+    print('Error exception: {}'.format(err))
+    print('Next try in {}ms'.format(next_try))
+
+#  Function to lookup Domains
+@riprova.retry(backoff=riprova.ExponentialBackOff(interval=0.5, factor=0.5, max_interval=60, max_elapsed=900, multiplier=1.5),on_retry=on_retry)
+def lookup_domains(vt_lookup_value):
+    list_to_return = []
+    url = 'https://www.virustotal.com/vtapi/v2/domain/report'
+    parameters = {'domain': vt_lookup_value, 'apikey': api_key}
+
+    try:
+        response = urllib.request.urlopen('%s?%s' % (url, urllib.parse.urlencode(parameters))).read()
+        time.sleep(cfg.request_wait)
+        response_json = json.loads(response)
+
+        vt_status = response_json['verbose_msg']
+        vt_ip = ''
+        vt_date = ''
+        vt_subdomain = ''
+        vt_url = ''
+        vt_score = ''
+        list_to_return = [(vt_lookup_value,vt_status,vt_ip,vt_date,vt_subdomain,vt_url,vt_score)]
+        vt_status = ''
+
+        try:
+            if response_json['resolutions']:
+                for i in response_json['resolutions']:
+                    vt_ip = response_json['resolutions'][0]['ip_address']
+                    vt_date = response_json['resolutions'][0]['last_resolved']
+                    list_to_return.append((vt_lookup_value, vt_status, vt_ip, vt_date, vt_subdomain, vt_url, vt_score))
+                vt_ip = ''
+                vt_date = ''
+        except KeyError:
+            pass
+
+        try:
+            if response_json['subdomains']:
+                for i in response_json['subdomains']:
+                    vt_subdomain = i
+                    list_to_return.append((vt_lookup_value, vt_status, vt_ip, vt_date, vt_subdomain, vt_url, vt_score))
+                vt_subdomain = ''
+        except KeyError:
+            pass
+
+        try:
+            if response_json['detected_urls']:
+                for i in response_json['detected_urls']:
+                    vt_url = i['url']
+                    vt_date = i['scan_date']
+                    vt_score = i['positives']
+                    list_to_return.append((vt_lookup_value, vt_status, vt_ip, vt_date, vt_subdomain, vt_url, vt_score))
+        except KeyError:
+            pass
+
+    except urllib.error.URLError as e:
+        vt_status = 'ERROR: ' +  e.reason
+        list_to_return = [(vt_lookup_value, vt_status, vt_date, vt_hostname, vt_url, vt_score)]
+
+    return list_to_return
+
+#  Function to lookup URLs
+@riprova.retry(backoff=riprova.ExponentialBackOff(interval=0.5, factor=0.5, max_interval=60, max_elapsed=900, multiplier=1.5),on_retry=on_retry)
+def lookup_urls(vt_lookup_value):
+    list_to_return = []
+    url = 'https://www.virustotal.com/vtapi/v2/url/report'
+
+    if vt_lookup_value[-1::] is '/':
+        parameters = {'apikey': api_key, 'resource': vt_lookup_value[:-1:]}
+    else:
+        parameters = {'apikey': api_key, 'resource': vt_lookup_value}
+
+    try:
+        response = urllib.request.urlopen('%s?%s' % (url, urllib.parse.urlencode(parameters))).read()
+        time.sleep(cfg.request_wait)
+        response_json = json.loads(response)
+
+        try:
+            vt_score = response_json['positives']
+            vt_date = response_json['scan_date']
+            vt_status = response_json['verbose_msg']
+            vt_url = response_json['url']
+            list_to_return = [(vt_lookup_value,vt_score,vt_date,vt_status,vt_url)]
+        except KeyError:
+            pass
+
+    except urllib.error.URLError as e:
+        vt_status = 'ERROR: ' +  e.reason
+        list_to_return = [(vt_lookup_value,vt_status,vt_date,vt_hostname,vt_url,vt_score)]
+
+    return list_to_return
+
+#  Function to force-lookup URLs
+@riprova.retry(backoff=riprova.ExponentialBackOff(interval=0.5, factor=0.5, max_interval=60, max_elapsed=900, multiplier=1.5),on_retry=on_retry)
+def force_urls(vt_lookup_value):
+    time.sleep(cfg.request_wait)
+    url = 'https://www.virustotal.com/vtapi/v2/url/scan'
+
+    if vt_lookup_value[-1::] is '/':
+        parameters = {'apikey': api_key, 'url': vt_lookup_value[:-1:]}
+    else:
+        parameters = {'apikey': api_key, 'url': vt_lookup_value}
+
+    data = urllib.parse.urlencode(parameters)
+    data = data.encode('UTF-8')
+
+    response = urllib.request.Request(url,data)
+    with urllib.request.urlopen(response) as response:
+        the_page = response.read()
+    response_json = json.loads(the_page)
+
+    try:
+        vt_score = ''
+        vt_date = response_json['scan_date']
+        vt_status = response_json['verbose_msg']
+        vt_url = response_json['url']
+        list_to_return = [(vt_lookup_value, vt_score, vt_date, vt_status, vt_url)]
+    except KeyError:
+        pass
+
+    list_to_return.extend(lookup_urls(vt_lookup_value))
+
+    return list_to_return
+
+#  Function to lookup IPs
+def lookup_ips(passed_value):
+    lookup_list = ipaddress.ip_network(passed_value, strict=False)
+    list_to_return = []
+    for vt_lookup_value in lookup_list:
+        print('Looking up', vt_lookup_value, end='..')
+        list_to_return.extend(lookup_slash_ips(vt_lookup_value)) #  Call subroutine
+    return list_to_return
+
+#  Subroutine to step through "slash" IP networks
+@riprova.retry(backoff=riprova.ExponentialBackOff(interval=2.5, factor=0.5, max_interval=60, max_elapsed=cfg.max_wait, multiplier=1.5),on_retry=on_retry)
+def lookup_slash_ips(vt_lookup_value):
+    loop_list = []
+    url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
+    parameters = {'ip': vt_lookup_value, 'apikey': api_key}
+
+    try:
+        print(".",end='')
+        response = urllib.request.urlopen('%s?%s' % (url, urllib.parse.urlencode(parameters))).read()
+        time.sleep(cfg.request_wait)
+        response_json = json.loads(response)
+        vt_status = response_json['verbose_msg']
+        vt_date = ''
+        vt_hostname = ''
+        vt_url = ''
+        vt_score = ''
+        loop_list = [(vt_lookup_value, vt_status, vt_date, vt_hostname, vt_url, vt_score)]
+        print(vt_status)
+        vt_status = ''
+
+        try:
+            if response_json['resolutions']:
+                for i in response_json['resolutions']:
+                    vt_date = str(i['last_resolved'])
+                    vt_hostname = str(i['hostname'])
+                    loop_list.append((vt_lookup_value, vt_status, vt_date, vt_hostname, vt_url, vt_score))
+                vt_date = ''
+                vt_hostname = ''
+        except KeyError:
+            pass
+
+        try:
+            if response_json['detected_urls']:
+                for i in response_json['detected_urls']:
+                    vt_date = str(i['scan_date'])
+                    vt_url = str(i['url'])
+                    vt_score = str(i['positives'])
+                    loop_list.append((vt_lookup_value, vt_status, vt_date, vt_hostname, vt_url, vt_score))
+                vt_date = ''
+                vt_url = ''
+                vt_score = ''
+        except KeyError:
+            pass
+
+        return loop_list
+
+    except urllib.error.URLError as e:
+        vt_status = 'ERROR: ' +  e.reason
+        loop_list.append((vt_lookup_value, vt_status, vt_date, vt_hostname, vt_url, vt_score))
+        return loop_list
+
+#  A little date formatter for pretty printing the job timer
+def td_format(td_object):
+    seconds = int(td_object.total_seconds())
+    periods = [
+        ('year', 60 * 60 * 24 * 365),
+        ('month', 60 * 60 * 24 * 30),
+        ('day', 60 * 60 * 24),
+        ('hour', 60 * 60),
+        ('minute', 60),
+        ('second', 1)
+    ]
+
+    strings = []
+    for period_name, period_seconds in periods:
+        if seconds >= period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            if period_value == 1:
+                strings.append("%s %s" % (period_value, period_name))
+            else:
+                strings.append("%s %ss" % (period_value, period_name))
+
+    return ", ".join(strings)
---
+++ vtbatch.py
@@ -0,0 +1,113 @@
+#  Python 3.6 modules
+import csv
+
+#  MIT licensed 3rd party modules
+import arrow
+
+#  import functions from vt_function.py to validate types and perform lookups
+import vt_functions as vt
+
+#  Globals
+final_results  = []
+
+
+#  Main program
+#  Prompt the user to choose what kind of lookup to perform
+while True:
+    try:
+        filetype = int(input("\n\nYou MUST have a file named VTlookup.txt in the same folder as the VTLookup.py program. \n"
+                     "The file MUST be one of the three types below: \n\n"
+                     "(1) Domain Names             **(no http:// or https:// on any of the lines)\n"
+                     "(2) URLs (lookup)            **(all lines must start with http:// or https://)\n"
+                     "(3) URLs (force a scan)      **(all lines must start with http:// or https://)\n"
+                     "(4) IP Addresses             **(only IP addresses; 'slash' notation is allowed, e.g. 192.168.0.0/16)\n\n"
+                     "Please enter the number for the type of file: "))
+    except ValueError:
+        print('\n***Please type a number: 1, 2, or 3***')
+        continue
+
+    if not filetype <5:
+        print('\n***Please type a number: 1, 2, or 3***')
+        continue
+
+    else:
+        break
+
+#  Read in the file for processing
+suspects = open('VTlookup.txt', 'r')
+list_to_process = [line.strip() for line in suspects]
+suspects.close()
+
+#  Validate that the file has only one type of lookup value and report back to the user if there are problems
+errors = 0
+filetype_text = {1:'a Domain Name',2:'a URL',3:'an IP Address'}
+for index, lookup_value in enumerate(list_to_process):
+    lookup_value = lookup_value.strip()
+
+    if vt.is_ip(lookup_value) is True:
+        if filetype is not 4:
+            print("Line ", index + 1, " is an IP address, not", filetype_text.get(filetype))
+            errors +=1
+
+    elif vt.is_url(lookup_value) is True:
+        if filetype is not 2 and filetype is not 3:
+            print("Line ", index + 1, " is a URL, not", filetype_text.get(filetype))
+            errors += 1
+
+    elif vt.is_domain(lookup_value) is True:
+        if filetype is not 1:
+            print("Line ", index + 1, " is a Domain Name, not", filetype_text.get(filetype))
+            errors += 1
+
+    else:
+        print("Line ", index + 1, " is not a valid Domain Name or IP or URL")
+        errors += 1
+
+if errors > 0:
+    print('\n***The',errors,'lines above need to be corrected before this file can be processed***')
+    exit()
+else:
+    print('Starting lookups...')
+
+#  Set up the header row in the final_results list and tell the program which function to call for each case
+if filetype is 1:
+    final_results = [('Lookup Value', 'VT Result Status', 'IP', 'Date', 'Observed Subdomains', 'URLs', 'Score')]
+    spacer = [('', '', '', '', '', '', '')]
+    def lookup(domain):
+        return vt.lookup_domains(domain)
+elif filetype is 2:
+    final_results = [('Lookup Value', 'Score', 'Date', 'VT Result Status', 'URL')]
+    spacer = []
+    def lookup(url):
+        return vt.lookup_urls(url)
+elif filetype is 3:
+    final_results = [('Lookup Value', 'Score', 'Date', 'VT Result Status', 'URL')]
+    spacer = []
+    def lookup(url):
+        return vt.force_urls(url)
+elif filetype is 4:
+    final_results = [('Lookup Value', 'VT Result Status', 'Date', 'A Record', 'URLs', 'Score')]
+    spacer = [('', '', '', '', '', '')]
+    def lookup(ip):
+        return vt.lookup_ips(ip)
+
+#  Do the lookups
+job_start_time = arrow.now()
+outof = len(list_to_process)
+
+for index, item in enumerate(list_to_process):
+    print('Looking up ', index + 1, 'out of', outof, ':', item)
+    final_results.extend(lookup(item))
+    final_results.extend(spacer)
+
+job_finish_time = arrow.now()
+print("Ended job at: ", arrow.utcnow().format('YYYY-MM-DD HH:mm:ss'), "and took ",
+      vt.td_format(job_finish_time-job_start_time),"to run.")
+
+#  Write it out to a timestamped results file (excel flavoured csv)
+csvname = "VT Lookup Results " +  arrow.now().format('YYYY-MM-DD HH-mm-a')+".csv"
+with open(csvname, 'w',newline='') as csvfile:
+    writer = csv.writer(csvfile, dialect='excel')
+    writer.writerows(final_results)
+    csvfile.flush()
+csvfile.close()
