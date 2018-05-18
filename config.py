1e0cfd9a9d48f3944546a457b19b57867cfa0c85
Adam Z. Wasserman <adam@adamwasserman.net>
2018-05-18 13:43:38 -0400

    Delete config.py which was renamed to vtconfig.py

--- config.py
+++
@@ -1,9 +0,0 @@
-#  VirusTotal API Key
-api_key          = 'put_APIKEY_here'
-
-#  Time between requests (to try an not trigger the VT rate limiter if using a Public api_key)
-request_wait = 0
-
-#  maximum time (in seconds) to wait for VT to respond before giving up 
-#  (in the case of the VT rate limiter kicking in)
-max_wait = 600
